afef0097f433915bb6e77d4a8031f75f248a6e08
abonneth <206544678+abonneth@users.noreply.github.com>
2026-06-15 15:56:11 +0000

    chore(sdk): sync to agent_platform@3b6de32 (v0.1.10)

---
+++ packages/sdk/src/client/api/resources/webhooks/client/requests/index.ts
@@ -0,0 +1,5 @@
+export type { CreateWebhook } from "./CreateWebhook.js";
+export type { DeleteWebhookRequest } from "./DeleteWebhookRequest.js";
+export type { GetWebhookRequest } from "./GetWebhookRequest.js";
+export type { ListWebhooksRequest } from "./ListWebhooksRequest.js";
+export type { UpdateWebhook } from "./UpdateWebhook.js";
